79408918d57f67a2b3f1fdbb6fe08a92b2274cf8
Lorrayne Firme <lorraynefirme@gmail.com>
2025-05-07 16:32:59 -0300

    feat: arquivo main

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/lorraynefirme/api-golang/src/controller/routes"
 )
-
 func main() {
-	erro := godotenv.Load()
-	if erro != nil {
+	err := godotenv.Load()
+	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	fmt.Println(os.Getenv("TEST"))
+	//fmt.Println(os.Getenv("TEST"))
+	router := gin.Default()
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
